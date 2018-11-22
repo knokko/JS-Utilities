36534ffcdb8ba9f0e7d213e9e648a92450fd4cd1
knokko <knokogator@hotmail.com>
2018-11-22 21:03:03 +0100

    Add console in case there is no console

---
+++ Console.js
@@ -0,0 +1,9 @@
+if(window.console === undefined) {
+  window.console = {
+    log : function(){},
+    warn : function(){},
+    error : function(){},
+    time : function(){},
+    timeEnd : function(){}
+  };
+}
